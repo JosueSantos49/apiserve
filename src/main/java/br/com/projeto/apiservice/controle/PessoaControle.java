68773810e4b5cc359239a3a5f81f5aa9c6766954
JosueSantos49 <conceicaojosue@yahoo.com.br>
2023-09-11 17:40:15 -0300

    Metodo criar pessoa no controle

--- src/main/java/br/com/projeto/apiservice/controle/PessoaControle.java
+++ src/main/java/br/com/projeto/apiservice/controle/PessoaControle.java
@@ -3,8 +3,12 @@ package br.com.projeto.apiservice.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.apiservice.modelo.Pessoa;
@@ -17,12 +21,20 @@ import lombok.AllArgsConstructor;
 public class PessoaControle {
 
     @Autowired
-    private final PessoaRepositorio pessoaRepositorio;
+    private PessoaRepositorio pessoaRepositorio;
 
     //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Pessoa> lista(){
         return pessoaRepositorio.findAll();
+    }
+
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Pessoa criar(@RequestBody Pessoa pessoa) {
+        return pessoaRepositorio.save(pessoa);
+        //return ResponseEntity.status(HttpStatus.CREATED).body(pessoaRepositorio.save(pessoa));
     }
 
 
--- src/main/java/br/com/projeto/apiservice/repositorio/ProdutoRepositorio.java
+++ src/main/java/br/com/projeto/apiservice/repositorio/ProdutoRepositorio.java
@@ -1,12 +1,12 @@
 package br.com.projeto.apiservice.repositorio;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.projeto.apiservice.modelo.Produto;
 
 @Repository
-public interface Repositorio extends CrudRepository<Produto, Long>{
+public interface ProdutoRepositorio extends JpaRepository<Produto, Long>{
     
     
 }
